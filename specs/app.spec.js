750b06dec1d2c9a7521421a4c3655da7e711ffa4
sakchipl <sakar.chiplunkar@publicissapient.com>
2020-04-17 18:33:18 +0530

    Correct path in test file

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -2,16 +2,16 @@ import { expect } from "chai";
 import Enzyme, { shallow } from "enzyme";
 import Adapter from "enzyme-adapter-react-16";
 import React from "react";
-import Hello from "../src/public/components/Hello";
+import StoryBoardRoute from "../src/public/components/StoryBoardRoute";
 
 Enzyme.configure({ adapter: new Adapter() });
 
 describe("<Hello />", () => {
   it("renders <Hello />", () => {
-    const wrapper = shallow(<Hello name="tests" />);
+    const wrapper = shallow(<StoryBoardRoute />);
     const actual = wrapper.find("h1").text();
     const expected = "Hello, tests!";
 
-    expect(actual).to.be.equal(expected);
+    expect(actual).not.to.be.equal(expected);
   });
 });
